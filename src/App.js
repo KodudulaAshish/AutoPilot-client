a7b04f76f46f9e20f143b8f4750227c26331662e
Ashish <kodudula3@gmail.com>
2024-10-06 21:59:23 +0530

    Squashed all changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ChatPane from './components/chatPane';
+
+function App() {
+  return (
+    <div className='container'>
+      <ChatPane/>
+    </div>
+  );
+}
+
+export default App;
